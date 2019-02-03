f946e3d33d0ff442c99914d61aeef3373abe61bd
sikirumoshood <sikirumoshood@gmail.com>
2019-02-03 13:38:01 +0100

    redeploy

--- server.js
+++ server.js
@@ -42,10 +42,10 @@ app.use("/api/post", post);
 //Connect server to static asset in client
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(__dirname, { index: "client/build/" }));
+  app.use(express.static("client/build/"));
+
   app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
 app.listen(port, () => console.log(`Server running on port ${port}...`));
